250931572f13b45d5b6097a668ddf1671df18195
htchepannou <htchepannou@expedia.com>
2021-03-17 08:05:16 -0400

    fix kotlin

--- src/main/kotlin/com/wutsi/like/model/CreateLikeRequest.kt
+++ src/main/kotlin/com/wutsi/like/model/CreateLikeRequest.kt
@@ -4,6 +4,6 @@ import javax.validation.constraints.NotBlank
 import kotlin.String
 
 public data class CreateLikeRequest(
-  @get:NotBlank
-  public val link: String? = null
+    @get:NotBlank
+    public val link: String? = null
 )
