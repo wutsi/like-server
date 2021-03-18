d158e19c6cdb9f861c536c8e65aa8db08f317133
htchepannou <htchepannou@expedia.com>
2021-03-18 19:49:56 -0400

    redeploy

--- codegen.sh
+++ codegen.sh
@@ -4,7 +4,10 @@ java -jar ../wutsi-codegen/target/wutsi-codegen-0.0.11.jar server \
     -g wutsi \
     -i https://wutsi-openapi.s3.amazonaws.com/like_api.yaml \
     -o . \
-    -heroku wutsi-test-like-service
+    -heroku wutsi-test-like-service \
+    -service_cache \
+    -service_logger \
+    -service_database
 
 mvn antrun:run@ktlint-format
 mvn antrun:run@ktlint-format
