f742f7131c3f55e00055a375d3939a1cc0f1ae65
htchepannou <htchepannou@expedia.com>
2021-03-20 11:19:09 -0400

    integrate db configurat

--- src/main/kotlin/com/wutsi/like/Application.kt
+++ src/main/kotlin/com/wutsi/like/Application.kt
@@ -1,11 +1,13 @@
 package com.wutsi.like
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import kotlin.String
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Application
 
-public fun main(vararg args: String) {
+fun main(args: Array<String>) {
     org.springframework.boot.runApplication<Application>(*args)
 }
