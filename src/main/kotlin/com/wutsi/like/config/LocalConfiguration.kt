e0535b434594e252107315582b1a20a04d1bd93a
htchepannou <htchepannou@expedia.com>
2021-03-21 10:47:04 -0400

    add caching configuration

---
+++ src/main/kotlin/com/wutsi/like/config/LocalConfiguration.kt
@@ -0,0 +1,23 @@
+package com.wutsi.like.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.concurrent.ConcurrentMapCache
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile(value = ["!test", "!prod"])
+class LocalConfiguration {
+    @Bean
+    fun cacheManager(): CacheManager {
+        val cacheManager = SimpleCacheManager()
+        cacheManager.setCaches(
+            listOf(
+                ConcurrentMapCache("default", true)
+            )
+        )
+        return cacheManager
+    }
+}
---
+++ src/main/kotlin/com/wutsi/like/config/MemcachedConfiguration.kt
@@ -0,0 +1,45 @@
+package com.wutsi.like.config
+
+import com.wutsi.spring.memcached.MemcachedCache
+import com.wutsi.spring.memcached.MemcachedClientBuilder
+import com.wutsi.spring.memcached.MemcachedHealthIndicator
+import net.rubyeye.xmemcached.MemcachedClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.cache.CacheManager
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile(value = ["test", "prod"])
+class MemcachedConfiguration(
+    @Value("\${memcached.username}") private val username: String,
+    @Value("\${memcached.password}") private val password: String,
+    @Value("\${memcached.addresses}") private val addresses: String,
+    @Value("\${memcached.ttl}") private val ttl: Int
+) {
+    @Bean
+    fun memcachedClient(): MemcachedClient =
+        MemcachedClientBuilder()
+            .withAddresses(addresses)
+            .withPassword(password)
+            .withUsername(username)
+            .build()
+
+    @Bean
+    fun memcachedHealthIndicator(): HealthIndicator =
+        MemcachedHealthIndicator(memcachedClient())
+
+    @Bean
+    fun cacheManager(): CacheManager {
+        val cacheManager = SimpleCacheManager()
+        cacheManager.setCaches(
+            listOf(
+                MemcachedCache("default", ttl, memcachedClient())
+            )
+        )
+        return cacheManager
+    }
+}
