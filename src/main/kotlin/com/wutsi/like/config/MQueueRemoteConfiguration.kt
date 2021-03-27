d68af5ecfaa96cfdda7aae61c1a9a46d169bde5b
htchepannou <htchepannou@expedia.com>
2021-03-27 17:56:10 -0400

    integration of rabbitmq

--- src/main/kotlin/com/wutsi/like/config/MQueueRemoteConfiguration.kt
+++ src/main/kotlin/com/wutsi/like/config/MQueueRemoteConfiguration.kt
@@ -1,16 +1,20 @@
 package com.wutsi.like.config
 
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.ConnectionFactory
 import com.wutsi.stream.Event
 import com.wutsi.stream.EventHandler
 import com.wutsi.stream.EventStream
 import com.wutsi.stream.rabbitmq.RabbitMQEventStream
+import com.wutsi.stream.rabbitmq.RabbitMQHealthIndicator
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.net.URI
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 @Configuration
@@ -23,15 +27,36 @@ class MQueueRemoteConfiguration(
     @Value(value = "\${rabbitmq.url}") private val url: String,
     @Value("\${rabbitmq.thread-pool-size}") val threadPoolSize: Int
 ) {
+    @Bean
+    fun connectionFactory(): ConnectionFactory {
+        val factory = ConnectionFactory()
+        factory.setUri(url)
+        return factory
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    fun executorService(): ExecutorService =
+        Executors.newFixedThreadPool(threadPoolSize)
+
+    @Bean(destroyMethod = "close")
+    fun channel(): Channel {
+        val factory = connectionFactory()
+        val connection = factory.newConnection(executorService())
+        return connection.createChannel()
+    }
+
     @Bean(destroyMethod = "close")
     fun stream(): EventStream = RabbitMQEventStream(
         name = "like",
-        uri = URI(url),
-        executorService = Executors.newFixedThreadPool(threadPoolSize),
+        channel = channel(),
         handler = object : EventHandler {
             override fun onEvent(event: Event) {
                 eventPublisher.publishEvent(event)
             }
         }
     )
+
+    @Bean
+    fun rabbitMQHealthIndicator(): HealthIndicator =
+        RabbitMQHealthIndicator(channel = channel())
 }
