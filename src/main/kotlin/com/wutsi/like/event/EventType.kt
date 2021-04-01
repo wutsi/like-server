3218ee919359dc8fd06b0d0343f4759a19ecc7c7
htchepannou <htchepannou@expedia.com>
2021-04-01 16:46:56 -0400

    change the event URN

--- src/main/kotlin/com/wutsi/like/event/EventType.kt
+++ src/main/kotlin/com/wutsi/like/event/EventType.kt
@@ -5,8 +5,8 @@ import com.wutsi.blog.stream.EventType as LegacyEventType
 enum class EventType(
     val urn: String
 ) {
-    INVALID("urn:wutsi:like:2.0:invalid"),
-    LIKED("urn:wutsi:like:2.0:liked"),
+    INVALID("urn:event:wutsi:like:2.0:invalid"),
+    LIKED("urn:event:wutsi:like:2.0:liked"),
 
     LEGACY_LIKED(LegacyEventType.LIKED.urn),
     LEGACY_UNLIKED(LegacyEventType.UNLIKED.urn),
